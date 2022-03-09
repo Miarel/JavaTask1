8d40017bec16e82f914a00cc6ea02d59dba58b2c
Miarel <larsidova@mail.ru>
2022-03-09 12:13:37 +0500

    new feature

--- src/JavaTask_1.java
+++ src/JavaTask_1.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class JavaTask_1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.printf("Hello, %s", name);
     }
 }
